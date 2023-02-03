028d3adb21dfb2a9a818a57bd0ede3307ad488ce
zhangchunsheng <zhangchunsheng423@gmail.com>
2023-02-03 19:19:09 +0800

    update

--- spring-boot-tdengine/src/main/java/com/luomor/util/TDengineUtil.java
+++ spring-boot-tdengine/src/main/java/com/luomor/util/TDengineUtil.java
@@ -91,6 +91,54 @@ public class TDengineUtil {
         consumer.close();
     }
     
+    public static void main(String[] args) throws SQLException {
+        // HikariConfig config = new HikariConfig();
+        // // jdbc properties
+        // config.setJdbcUrl("jdbc:TAOS://127.0.0.1:6030/log");
+        // config.setUsername("root");
+        // config.setPassword("taosdata");
+        // // connection pool configurations
+        // config.setMinimumIdle(10);           //minimum number of idle connection
+        // config.setMaximumPoolSize(10);      //maximum number of connection in the pool
+        // config.setConnectionTimeout(30000); //maximum wait milliseconds for get connection from pool
+        // config.setMaxLifetime(0);       // maximum life time for each connection
+        // config.setIdleTimeout(0);       // max idle time for recycle idle connection
+        // config.setConnectionTestQuery("select server_status()"); //validation query
+    
+        // HikariDataSource ds = new HikariDataSource(config); //create datasource
+    
+        // Connection  connection = ds.getConnection(); // get connection
+        // Statement statement = connection.createStatement(); // get statement
+    
+        // //query or insert
+        // // ...
+    
+        // connection.close(); // put back to conneciton pool
+    }
+
+    public static void main1(String[] args) throws Exception {
+
+        // DruidDataSource dataSource = new DruidDataSource();
+        // // jdbc properties
+        // dataSource.setDriverClassName("com.taosdata.jdbc.TSDBDriver");
+        // dataSource.setUrl(url);
+        // dataSource.setUsername("root");
+        // dataSource.setPassword("taosdata");
+        // // pool configurations
+        // dataSource.setInitialSize(10);
+        // dataSource.setMinIdle(10);
+        // dataSource.setMaxActive(10);
+        // dataSource.setMaxWait(30000);
+        // dataSource.setValidationQuery("select server_status()");
+    
+        // Connection  connection = dataSource.getConnection(); // get connection
+        // Statement statement = connection.createStatement(); // get statement
+        // //query or insert
+        // // ...
+    
+        // connection.close(); // put back to conneciton pool
+    }
+    
     /**
      * # first fully qualified domain name (FQDN) for TDengine system
         firstEp               cluster_node1:6030
