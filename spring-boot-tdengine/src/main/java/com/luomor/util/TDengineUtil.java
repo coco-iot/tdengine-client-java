7a61eda9ea24a72d0f60e0bf1bea20ebb93668b4
zhangchunsheng <zhangchunsheng423@gmail.com>
2023-02-03 18:42:44 +0800

    update

--- spring-boot-tdengine/src/main/java/com/luomor/util/TDengineUtil.java
+++ spring-boot-tdengine/src/main/java/com/luomor/util/TDengineUtil.java
@@ -10,13 +10,13 @@ import com.taosdata.jdbc.TSDBDriver;
 public class TDengineUtil {
     public void test() throws ClassNotFoundException, SQLException {
         Class.forName("com.taosdata.jdbc.TSDBDriver");
-        String jdbcUrl = "jdbc:TAOS://taosdemo.com:6030/test?user=root&password=taosdata";
+        String jdbcUrl = "jdbc:TAOS://localhost:6030/test?user=root&password=taosdata";
         Connection conn = DriverManager.getConnection(jdbcUrl);
     }
     
     public Connection getConn() throws Exception{
         Class.forName("com.taosdata.jdbc.TSDBDriver");
-        String jdbcUrl = "jdbc:TAOS://:/test?user=root&password=taosdata";
+        String jdbcUrl = "jdbc:TAOS://localhost:6030/test?user=root&password=taosdata";
         Properties connProps = new Properties();
         connProps.setProperty(TSDBDriver.PROPERTY_KEY_CHARSET, "UTF-8");
         connProps.setProperty(TSDBDriver.PROPERTY_KEY_LOCALE, "en_US.UTF-8");
